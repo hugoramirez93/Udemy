7c3ea831a8624f4517d985e12eeb39c4d952f191
hugoramirez93 <hugoo.ramirez@outlook.com>
2025-06-10 17:30:05 -0500

    1st Challenge done

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,95 @@
+// let js = "amazing";
+// console.log(40+8+23-10);
+
+// // Values and Variables
+// console.log("Jonas");
+// console.log(23);
+
+// let firstName = "Hugo";
+// console.log(firstName);
+
+
+// const country = "Colombia";
+// const continent = "America";
+// let population = 50000000;
+// const isIsland = false;
+// const language = "";
+// let finlandPopulation = 6000000;
+// console.log(country);
+// console.log(continent);
+// console.log(population);
+// console.log(isIsland);
+// console.log(language);
+
+// console.log(country / 2);
+
+// population++;
+
+/*let finlandPopulation = 6000000;
+let population = 50000000;
+
+
+if (population > finlandPopulation){
+    console.log("That is true.")
+} else {
+    console.log("That is false.")
+};
+*/
+
+
+// let x = 10 + 5; // 15
+// x += 10; // x = x + 10 = 25
+// x *= 4; // x = x * 4 = 100
+// x ++; // x = x + 1
+// x --; // x = x - 1
+// x --; // x = x - 1
+// console.log(x);
+
+// Comparison operators
+// console.log(ageJones > ageSarah); // >, <, >=, <=
+// console.log(ageSarah >= 18);
+
+// const isFullAge = ageSarah >= 18;
+
+// const now = 2037;
+// const ageJonas = now - 1991;
+// const ageSarah = now - 2018;
+
+// console.log(now - 1991 > now - 2018);
+
+// let x, y;
+// x = y = 25 - 10 - 5; //x = y = 10, x = 10
+// console.log(x, y);
+
+// const averageAge = (ageJonas + ageSarah) / 2;
+// console.log(ageJonas, ageSarah);
+
+
+// CHALLENGE PART 1
+
+let markKgs = 78;
+let markHeight = 169 / 100;
+let johnKgs = 92;
+let johnHeight = 188 / 100;
+
+let markHeightTotal = markHeight * markHeight;
+let markBMI = (markKgs / markHeightTotal);
+
+let johnHeightTotal = johnHeight * johnHeight;
+let johnBMI = (johnKgs / johnHeightTotal);
+
+
+console.log ("The BMI of Mark is " + markBMI);
+console.log ("The BMI of John is " + johnBMI);
+
+let markHigherBMI = markBMI > johnBMI;
+
+console.log(markHigherBMI);
+
+if (johnBMI > markBMI) {
+    console.log ("Jhon has more BMI than Mark.")
+} else {
+    console.log ("Mark has more BMI than Jhon.")
+};
+
+
